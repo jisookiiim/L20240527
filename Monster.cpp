4818325f842f44235b2b21c5fe92474fa1923345
jisookim <jisookimwork@gmail.com>
2024-05-27 14:15:34 +0900

    public과 private, protected

---
+++ Monster.cpp
@@ -0,0 +1,36 @@
+#include "Monster.h"
+
+#include <iostream>
+
+using namespace std;
+
+FMonster::FMonster()
+{
+	cout << "FMonster Constructor" << endl;
+}
+
+FMonster::~FMonster()
+{
+	cout << "FMonster Destructor" << endl;
+}
+
+void Move()
+{
+}
+
+void Attack()
+{
+}
+
+void Pickup()
+{
+}
+
+int GetGold()
+{
+	return 0;
+}
+
+void SetGold(int NewGold)
+{
+}
